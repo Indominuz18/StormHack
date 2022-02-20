e10c687f8531c968fd5c74a54c48e2aaa8f0715a
eth-p <eth-p+git@hidden.email>
2022-02-19 20:11:58 -0800

    Fix env vars not being picked up

--- web/src/config.tsx
+++ web/src/config.tsx
@@ -3,10 +3,10 @@
  */
 export const Config = {
 	project: {
-		name: process.env.PROJECT_NAME ?? "Calendar",
+		name: process.env.REACT_APP_PROJECT_NAME ?? "Calendar",
 	},
 
 	url: {
-		api: process.env.API_SERVER ?? "shim://dev",
+		api: process.env.REACT_APP_API_SERVER ?? "shim://dev",
 	}
 }
